8b4e23ecb61494fe7c7717225708fa927d1b7c97
Shahroz Ahmad <shahroz.ahmad@arbisoft.com>
2022-04-05 00:07:24 +0500

    Added Django Rest API for Room API

--- djangopractice/settings.py
+++ djangopractice/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
