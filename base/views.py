e076f20f9f22f5c4312558e1b19a080e96b212b3
Shahroz Ahmad <shahroz.ahmad@arbisoft.com>
2022-04-04 16:08:05 +0500

    Added Home and Room templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request, 'room.html')
+
