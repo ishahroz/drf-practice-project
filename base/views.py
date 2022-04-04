0495c4cdee06f8421a4ac6b8c901dc0c84a77d84
Shahroz Ahmad <shahroz.ahmad@arbisoft.com>
2022-04-04 16:18:16 +0500

    Moving templates to base app level

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me!'},
+    {'id': 3, 'name': 'Front-end Developers'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
-
+    return render(request, 'base/room.html')
